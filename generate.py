353287c01aab5379fcd367295e34d80433318dfd
hackedXD <sidharth.a.mahesh@gmail.com>
2022-10-03 18:54:35 +0800

    include all

--- generate.py
+++ generate.py
@@ -25,8 +25,37 @@ if __name__ == "__main__":
 
 
 
-    html = ""
+    cards = ""
     temp_html = """
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <title>Bootstrap demo</title>
+            <link
+                href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+                rel="stylesheet"
+                integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+                crossorigin="anonymous"
+            />
+        </head>
+        <body>
+            <div class="container text-center">
+                <div class="row row-cols-4">
+                {cards}
+                </div>
+            </div>
+
+            <script
+                src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+                crossorigin="anonymous"
+            ></script>
+        </body>
+    </html>
+    """
+    temp_card = """
         <div class="col">
             <div class="card my-3">
                 <img
@@ -44,11 +73,12 @@ if __name__ == "__main__":
             room = teacher["room"]
         except:
             room = "N/A"
-        html += temp_html.format(
+        cards += temp_card.format(
             image=teacher["photo"],
             email=teacher["email"],
             room=room,
         )
 
-    print(html)
+    with open("index.html", "w") as f:
+        f.write(temp_html.format(cards=cards))
 
